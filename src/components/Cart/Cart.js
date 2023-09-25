572d86a4fbaf78005b8ccaf61c991f406f1fbadd
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-09-25 21:13:20 +0300

    Finish Cart Logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,45 @@
+import { useContext } from "react";
+
 import CartItem from "./CartItem";
 import Modal from "../UI/Modal";
 
 import stylesCart from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
-  const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }];
+  const cartContext = useContext(CartContext);
+
+  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+  const hasItems = cartContext.items.length > 0;
+ 
+  const cartItemRemoveHandler = (id) => {
+    cartContext.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartContext.addItem({ ...item, amount: 1 });
+  };
 
   return (
     <Modal hideCart={props.hideCart}>
       <ul className={stylesCart.cartItems}>
-        {cartItems.map((i) => (
-          <CartItem key={i.id} item={i} />
+        {cartContext.items.map((i) => (
+          <CartItem
+            key={i.id}
+            item={i}
+            onRemove={cartItemRemoveHandler}
+            onAdd={cartItemAddHandler}
+          />
         ))}
       </ul>
       <div className={stylesCart.total}>
         <span>Total Amount</span>
-        <span>69</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={stylesCart.actions}>
         <button className={stylesCart.buttonAlt} onClick={props.hideCart}>
           Close
         </button>
-        <button className={stylesCart.button}>Order</button>
+        {hasItems && <button className={stylesCart.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { Fragment, useContext, useState } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 
@@ -6,15 +6,34 @@ import styles from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighLighted, setButtonIsHighLighted] = useState(false);
   const cartContext = useContext(CartContext);
-  
+
   const numberOfCartItems = cartContext.items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setButtonIsHighLighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighLighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
   return (
     <Fragment>
-      <button onClick={props.onClick} className={styles.button}>
+      <button
+        onClick={props.onClick}
+        className={`${styles.button} ${buttonIsHighLighted && styles.bump}`}
+      >
         <span className={styles.icon}>
           <CartIcon />
         </span>
