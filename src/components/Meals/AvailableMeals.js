bd03f35f0cc408dcf87ef9565ad05ba081b64c38
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-10-04 13:18:40 +0300

    FetchData from Firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,18 +1,54 @@
+import { useEffect, useState } from "react";
+
 import Card from "../UI/Card";
 import MealItem from "./MealItem";
 
-import meals from "../../assets/dummy-meals";
 import styles from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
+
+  const fetchMealsHandler = async () => {
+    try {
+      const response = await fetch(
+        "https://react-http-cf2f1-default-rtdb.firebaseio.com/meals.json"
+      );
+      const data = await response.json();
+
+      const loadedMeals = [];
+      for (const mealKey in data) {
+        loadedMeals.push({
+          id: mealKey,
+          name: data[mealKey].name,
+          description: data[mealKey].description,
+          price: data[mealKey].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMealsHandler();
+  }, []);
+
   return (
     <section className={styles.meals}>
       <Card>
-        <ul>
-          {meals.map((m) => (
-            <MealItem meal={m} key={m.id} />
-          ))}
-        </ul>
+        {isloading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {meals.map((m) => (
+              <MealItem meal={m} key={m.id} />
+            ))}
+          </ul>
+        )}
       </Card>
     </section>
   );
